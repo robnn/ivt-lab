4df793aae97b3ba8f9ec52aedb28838a2d2e5455
robin.kurovszky@gmail.com <robin.kurovszky@gmail.com>
2017-04-28 04:38:46 -0700

    lab6

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -10,31 +10,113 @@ public class GT4500Test {
 
   private GT4500 ship;
 
+  private TorpedoStore first;
+  private TorpedoStore second;
   @Before
   public void init(){
-    this.ship = new GT4500();
+    first = mock(TorpedoStore.class);
+    second = mock(TorpedoStore.class);
+    this.ship = new GT4500(first,second);
   }
 
   @Test
   public void fireTorpedos_Single_Success(){
     // Arrange
-
+    when(first.isEmpty()).thenReturn(false);
+    when(second.isEmpty()).thenReturn(false);
+    when(first.fire(1)).thenReturn(true);
+    when(second.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedos(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+    verify(first,times(1)).fire(1);
+    verify(second,times(0)).fire(1);
   }
 
   @Test
   public void fireTorpedos_All_Success(){
     // Arrange
-
+    when(first.isEmpty()).thenReturn(false);
+    when(second.isEmpty()).thenReturn(false);
+    when(first.fire(1)).thenReturn(true);
+    when(second.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedos(FiringMode.ALL);
 
     // Assert
-    assertEquals(true, result);
+    verify(first,times(1)).fire(1);
+    verify(second,times(1)).fire(1);
+
+  }
+  //Elsüti az első torpedót, majd a másikat,SINGLE módban majd megnézi, hogy mindkettő
+  //elsült e egyszer.Mindkettő store egyet tartalmaz.
+  @Test
+  public void fireTwoTorpedosInSingleMode(){
+    when(first.isEmpty()).thenReturn(false);
+    when(second.isEmpty()).thenReturn(false);
+    when(first.fire(1)).thenReturn(true);
+    when(second.fire(1)).thenReturn(true);
+
+    ship.fireTorpedos(FiringMode.SINGLE);
+    ship.fireTorpedos(FiringMode.SINGLE);
+
+    verify(first,times(1)).fire(1);
+    verify(second,times(1)).fire(1);
   }
 
+  //Elsüti 2x SINgLE módban, de csak az egyik store-ban van 2 töltény, ellenőrizzük,
+  //hogy 2x hívodott e.
+  @Test
+  public void fireTwoTorpedosInSingleButOnlyOneStoreHasTorpedos(){
+    when(first.isEmpty()).thenReturn(false);
+    when(second.isEmpty()).thenReturn(true);
+    when(first.fire(1)).thenReturn(true);
+    when(second.fire(1)).thenReturn(false);
+
+    ship.fireTorpedos(FiringMode.SINGLE);
+    ship.fireTorpedos(FiringMode.SINGLE);
+
+    verify(first,times(2)).fire(1);
+    verify(second,times(0)).fire(1);
+  }
+  //Minden store üres és ALL módba tüzel, elvárt kimenet hogy nem sikerült hívjon 1x se
+  @Test
+  public void fireAllFailure(){
+    when(first.isEmpty()).thenReturn(true);
+    when(second.isEmpty()).thenReturn(true);
+    when(first.fire(1)).thenReturn(false);
+    when(second.fire(1)).thenReturn(false);
+
+    ship.fireTorpedos(FiringMode.ALL);
+
+    verify(first,times(0)).fire(1);
+    verify(second,times(0)).fire(1);
+  }
+  //Minden store üres és singleben tüzel, elvárt, hogy nem hívott egyet se
+  @Test
+  public void fireSingleFailure(){
+    when(first.isEmpty()).thenReturn(true);
+    when(second.isEmpty()).thenReturn(true);
+    when(first.fire(1)).thenReturn(false);
+    when(second.fire(1)).thenReturn(false);
+
+    ship.fireTorpedos(FiringMode.SINGLE);
+
+    verify(first,times(0)).fire(1);
+    verify(second,times(0)).fire(1);
+  }
+   //Üres secondary de ALL módban egyszer hívodott a primary és 0x a secondary
+  @Test
+  public void fireOneInAll(){
+    when(first.isEmpty()).thenReturn(false);
+    when(second.isEmpty()).thenReturn(true);
+    when(first.fire(1)).thenReturn(true);
+    when(second.fire(1)).thenReturn(false);
+
+    ship.fireTorpedos(FiringMode.ALL);
+
+    verify(first,times(1)).fire(1);
+    verify(second,times(0)).fire(1);
+  }
 }
